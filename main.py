2479bc25afe47f8fca1fc8f2d37f1afd5284bcab
Justin <JustinChen3946@gmail.com>
2023-09-16 15:44:10 -0400

    Added commit + error checking:

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ Basically the "CLI interface" you see
 '''
 
 import argparse
+import os
+
+def commit(message):
+    assert message != None, "You must provide a commit message"
+    os.system("git add -A")
+    os.system(f"git commit -a -m {message}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Initiates ACRYPT to encrypt all API keys and then commit changes.')
-    parser.add_argument('-m', '--manual', help='Do not commit changes', action='store_true')
+    parser.add_argument('-m', '--message', help="Commit message")
+    parser.add_argument('-n', '--nocommit', help='Do not commit changes', action='store_true')
     parser.add_argument('-u', '--undo', help='Undo an instance of ACRYPT', action='store_true')
 
     flags = vars(parser.parse_args())
